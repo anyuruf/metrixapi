1698785215f4627d5ea24f4b7ef8524a24c44e2a
anyuruf <anyuruf@gmail.com>
2021-12-07 12:18:05 +0300

    Setup cors and update package.json with keywords

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import neoSchema from './schema/neo-schema';
 
 const { ApolloServer } = require('apollo-server')
+const { cors } = require('cors')
 /*
  * Create a new Apollo Server instance, serving the GraphQL schema
  * created using Neo4jGraphQL from schema folder
  */
 const server = new ApolloServer({
   cors:{ 
-	origin: '*',
+	origin: 'http://localhost:4200',
 	credentials: true},
   context: ({ req }) => ({ req }),
   schema: neoSchema.schema,
