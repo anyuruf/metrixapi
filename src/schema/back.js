3411e77ba2f5f6c51ca7488fe8ff213e40e8fd8a
anyuruf <anyuruf@gmail.com>
2022-05-01 13:36:09 +0300

    Fix: clean schema

--- src/schema/back.js
+++
@@ -1,9 +0,0 @@
-extend type User @exclude @auth (rules: [
-       {operations: [CREATE,CONNECT, DISCONNECT], roles: ["zadmin", "admin"]},
-       {operations: [UPDATE, READ, DELETE],
-          OR: [{ roles: ["zadmin", "admin"] }, { allow: { id: "$jwt.sub" } }]}
-    ])
-
-@auth(rules: [{roles: ["zadmin", "admin"]}])
-
-@auth(rules: [{roles: ["zadmin", "admin"]}])
